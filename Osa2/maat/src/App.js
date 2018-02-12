3aac9edd6874cb29981db01c8e6555dcadc159df
Enzine <sonja.makipaja@kolumbus.fi>
2018-02-12 23:42:11 +0200

    viikko 3

--- Osa2/maat/src/App.js
+++ Osa2/maat/src/App.js
@@ -15,7 +15,6 @@ class App extends React.Component {
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        console.log(response.data[0].name)
         this.setState({ lands: response.data })
       })
   }
@@ -24,15 +23,21 @@ class App extends React.Component {
     this.setState({ search: event.target.value })
   }
 
+  handleLandsToShowChange = (land) => {
+    let newLandsToShow = []
+    newLandsToShow = newLandsToShow.push(land)
+    console.log(land.name)
+    this.setState({ landsToShow: newLandsToShow })
+    console.log(Array.isArray(this.state.landsToShow))    
+  }
+
   updateLandsToShow = (event) => {
     event.preventDefault()
     let newLandsToShow = this.state.lands.filter(land => land.name.toLowerCase().includes(this.state.search))
-    
+
     if (newLandsToShow.length > 10) {
       let tooManyMatches = [{name: 'Got too many matches, please specify another filter.'}]
       this.setState({ landsToShow: tooManyMatches })
-    } else if (this.state.search === '') {
-      this.setState({ landsToShow: this.state.lands })
     } else {
       this.setState({
         landsToShow: newLandsToShow,
@@ -42,6 +47,17 @@ class App extends React.Component {
   }
 
   render() {
+    let landsToShow = this.state.landsToShow
+    let f = landsToShow.map(land => <li key={land.name} onClick={() => this.handleLandsToShowChange(land)}>{land.name}</li>)
+    if (this.state.landsToShow.length === 1) {
+      const land = this.state.landsToShow[0]
+      f = <div>
+            <h2>{land.name}</h2>
+            Capital: {land.capital}<br/><br/>
+            Population: {land.population}<br/><br/>
+            <img src={land.flag} height="120" width="200"></img>
+          </div>
+    }
     return (
       <div>
         <form onSubmit={this.updateLandsToShow}>
@@ -52,9 +68,8 @@ class App extends React.Component {
           />
           <button type='submit'>search</button>      
         </form>
-
         <ul>
-          {this.state.landsToShow.map(land => <li key={land.name}>{land.name}</li>)}
+          {f}
         </ul>
       </div>
     )
