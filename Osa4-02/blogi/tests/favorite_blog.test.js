e6d683d84608c1e07c45159da44888e93b827c60
Enzine <sonja.makipaja@kolumbus.fi>
2018-02-25 22:03:48 +0200

    Week 4 ex. 5

---
+++ Osa4-02/blogi/tests/favorite_blog.test.js
@@ -0,0 +1,33 @@
+const listHelper = require('../utils/list_helper')
+
+describe('favorite blog', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithNoBlogs = []
+
+  test('when list has only one blog equals that one', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual({
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    })
+  })
+
+  test('when list has no blogs equals 0', () => {
+    const result = listHelper.favoriteBlog(listWithNoBlogs)
+    expect(result).toBe(0)
+  })
+})
--- Osa4-02/blogi/utils/list_helper.js
+++ Osa4-02/blogi/utils/list_helper.js
@@ -6,7 +6,13 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length > 0) return blogs.reduce((favorite, blog) => favorite.likes < blog.likes ? blog : favorite)
+  return 0
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
